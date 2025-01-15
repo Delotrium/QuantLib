c568985332d63102f4f0e7f8e6e1b98922f30c8b
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-16 01:44:42 +1000

    updated includes

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
+#include "GenAnalysis.h"
 #include <cmath>
 #include <string>
 #include "ProgramIO.h"
+
+
+
+
+
+
 int main()
 {
-	RunBinomCDF(); //Choose the function you want to run
+	std::cout <<  CompoundAnnualGrowthRate(1000000,1200000,5,0); //Choose the function you want to run
 	return 0;
 }
+
