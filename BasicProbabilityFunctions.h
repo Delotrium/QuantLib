8cb1889fe63e5cb30d97a0fde5a94ce0f5bfd0b6
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-16 01:44:04 +1000

    Added a variety of probability functions

--- BasicProbabilityFunctions.h
+++ BasicProbabilityFunctions.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 double BasicProbabilityRepeat(double ObservedEventProbability, double ObserveredSuccesses)
 {
@@ -37,4 +38,93 @@ double BinomCDF(double ObservedEventProbability, double numTrials, double lowerL
 		lowerLimit++;
 	}
 	return CDF;
+}
+
+double ExpectedValue(std::vector<double> values, std::vector<double> probabilities)
+{
+	int i = 0;
+	double val = 0;
+	while (i < values.size())
+	{
+		val = val + (values[i] * probabilities[i]);
+		i++;
+	}
+	return val;
+}
+
+double ExpectedValueBinom(double numTrials, double probSuccess)
+{
+	return numTrials * probSuccess;
+}
+
+double VarBinom(double numTrials, double probSucess)
+{
+	return numTrials * probSucess * (1 - probSucess);
+}
+
+double ArithmeticMean(std::vector<double> values)
+{
+	int i=0;
+	int val=0;
+	while (i < values.size())
+	{
+		val = val + values[i];
+		i++;
+	}
+	return (val / values.size());
+}
+
+double VarianceListSample(std::vector<double> values)
+{
+	
+	int i = 0;
+	double val = 0;
+	double arith = ArithmeticMean(values);
+	while (i < values.size())
+	{
+		val += (pow(values[i] - arith, 2));
+		i++;
+	}
+	return val / (values.size() - 1);
+}
+
+double VarianceListPop(std::vector<double> values)
+{
+
+	int i = 0;
+	double val = 0;
+	double arith = ArithmeticMean(values);
+	while (i < values.size())
+	{
+		val += (pow(values[i] - arith, 2));
+		i++;
+	}
+	return val / (values.size());
+}
+
+double StandardDeviatonSample(std::vector<double> values)
+{
+	return sqrt(VarianceListSample(values));
+}
+
+double StandardDeviatonPop(std::vector<double> values)
+{
+	return sqrt(VarianceListPop(values));
+}
+
+double geoMetricMean(std::vector<double> values)
+{
+	int i =0;
+	double val = 1;
+	while (i < values.size())
+	{
+		val *= values[i];
+		i++;
+	}
+	return pow(val, 1.0 / values.size());
+}
+
+double CompoundAnnualGrowthRate(double initValue, double endValue, double initTime, double endTime)
+{
+	return (pow(endValue / initValue, 1 / (endTime, initTime))) - 1;
 }
