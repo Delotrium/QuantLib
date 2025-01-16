483368e7b85b0245376e329ccf4194ad4179aa7d
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-17 01:30:13 +1000

    updated test

--- Source.cpp
+++ Source.cpp
@@ -11,8 +11,9 @@
 
 int main()
 {
-	std::cout << Skew({ 2,2,3,4,5 }) << std::endl;
-	std::cout << Kurtosis({ 2,2,3,4, 5 }); //Choose the function you want to run
+	std::vector<double> values = { 1,2,3,3,4,5,3,1,1,1,5,2,3,1,2,3,4,5,5,4,3,2,1,2,3,2,2,1,1 };//Choose the function you want to run
+	std::vector<double> results = DistributionAnalysisSimple(values);
+	std::cout << "Arithmetic Mean: " << results[0] << std::endl << "Standard Deviation: " << results[1] << std::endl << "Skew: " << results[2] << std::endl << "Kurtosis: " << results[3] << std::endl;
 	return 0;
 }
 
