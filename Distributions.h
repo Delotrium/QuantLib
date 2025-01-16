15687ae01fc56ffee46a020c4c23397cbd85e9a9
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-17 01:03:22 +1000

    added distributions

---
+++ Distributions.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <cmath>
+#include "BasicProbabilityFunctions.h"
+#include <string_view>
+
+double pi = 3.14159265358979323846;
+
+double Poisson(double lambda, int x)
+{
+	return (exp(-lambda) * pow(lambda, x)) / Factorial(x);
+}
+
+double GaussianDistr(double mean, double sd, double x)
+{
+	double GausDst = (1 / (sd * sqrt(2 * pi))) * exp((-pow(x - mean, 2) / (2 * pow(sd, 2))));
+	return GausDst;
+}
+
+double Skew(std::vector<double>values)
+{
+	double mean = ArithmeticMean(values);
+	double sd = StandardDeviatonSample(values);
+	double coeff = 1 / (values.size()*pow(sd,3));
+	double skew = 0;
+	int i = 0;
+	while (i < values.size())
+	{
+		skew += pow(values[i] - mean, 3);
+		i++;
+	}
+	return skew * coeff;
+}
+
+double Kurtosis(std::vector<double>values)
+{
+	double mean = ArithmeticMean(values);
+	double sd = StandardDeviatonSample(values);
+	double coeff = 1 / (values.size() * pow(sd, 4));
+	double skew = 0;
+	int i = 0;
+	while (i < values.size())
+	{
+		skew += pow(values[i] - mean, 4);
+		i++;
+	}
+	return skew * coeff - 3;
+}
