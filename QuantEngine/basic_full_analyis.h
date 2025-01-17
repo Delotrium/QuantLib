b767a9ff89706e09a30d8ea2cb8e9c68abe0bb6f
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-18 01:11:06 +1000

    added analysis engine

---
+++ QuantEngine/basic_full_analyis.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "QuantLib.h"
+
+namespace QuantEngine
+{
+	void analysis(std::vector<double> data);
+}
---
+++ QuantEngine/basic_full_analysis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "QuantLib.h"
+#include "QuantEngine.h"
+
+namespace QuantEngine
+{
+	void analysis(std::vector<double> data)
+	{
+		
+		double Mean = QuantLib::ArithmeticMean(data);
+		double StDev = QuantLib::StandardDeviatonPop(data);
+		double Variance = QuantLib::VarianceListPop(data);
+		double Skew = QuantLib::Skew(data);
+		double Kurt = QuantLib::Kurtosis(data);
+		double GeoMean = QuantLib::geoMetricMean(data);
+		std::cout << "Arithmetic Mean: " << Mean << std::endl << "Standard Deviation: " << StDev << std::endl << "Variance: " << Variance << std::endl << "Skew: " << Skew << std::endl << "Kurtosis: " << Kurt << std::endl << "Geometric Mean: " << GeoMean << std::endl;
+
+	}
+}
