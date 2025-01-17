55cad4eb9ab16fe0b0fecd8b6bd073a914c6d803
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-18 01:52:51 +1000

    updated test case

--- run.cpp
+++ run.cpp
@@ -7,11 +7,12 @@
 int main()
 {
 	int i = 0;
-	while (i < 20)
+	while (i < 1)
 	{
 		std::cout << "Trial: " << i << std::endl;
 		std::cout << "****************************************************************" << std::endl;
-		QuantEngine::analysis(QuantEngine::generate_points(0, 100, 500));
+		std::vector<double> data = QuantEngine::generate_points(0, 100, 100, 1);
+		QuantEngine::analysis(data);
 		std::cout << "****************************************************************" << std::endl << "\n\n\n";
 		i++;
 	}
