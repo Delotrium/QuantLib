3cc2f787780d815b1269baa56fcd64e4fba62bf6
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-19 19:55:29 +1000

    updated test case

--- run.cpp
+++ run.cpp
@@ -4,19 +4,11 @@
 
 
 
+
+
 int main()
 {
-	int i = 0;
-	while (i < 1)
-	{
-		std::cout << "Trial: " << i << std::endl;
-		std::cout << "****************************************************************" << std::endl;
-		std::vector<double> data = QuantEngine::generate_points(0, 100, 100, 1);
-		QuantEngine::analysis(data);
-		std::cout << "****************************************************************" << std::endl << "\n\n\n";
-		i++;
-	}
+	QuantEngine::SimpleRandomWalkGenerator();
 	return 0;
-
 }
 
