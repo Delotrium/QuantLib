65afe3be4221320ea99c74712c27f2e0a7ca6c0d
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2025-01-21 15:44:14 +1000

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -8,8 +8,8 @@
 
 int main()
 {
-	std::cout << "Calculated Call Price: $" << QuantLib::SimpleBlackScholes(100, 110, 1, 0.05, 0.2, true) << std::endl;
-	std::cout << "Calculated Put Price: $" << QuantLib::SimpleBlackScholes(100, 110, 1, 0.05, 0.2, false) << std::endl;
+	std::cout << "Calculated Call Price: " << QuantLib::Delta(80, 120, 0.5, 0.05, 0.3, 0.05, true) << std::endl;
+	std::cout << "Calculated Put Price: " << QuantLib::Delta(80, 120, 0.5, 0.05, 0.3, 0.05, false) << std::endl;
 	return 0;
 }
 
